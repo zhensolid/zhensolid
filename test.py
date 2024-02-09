3a83e8c3b180bdd95ea5debec5e0041e6ce31a7a
zhensolid <zhensolid@vip.qq.com>
2024-02-09 16:12:35 +0800

    update

---
+++ test.py
@@ -0,0 +1,12 @@
+# Created on iPad.
+import sys
+print ('Hello World!')
+a = int(input("测试数据a:"));
+b = int(input("测试数据b:"));
+if a < 0 or b < 0 :
+    print("输入错误");
+    sys.exit()
+print(a+b);
+
+
+
